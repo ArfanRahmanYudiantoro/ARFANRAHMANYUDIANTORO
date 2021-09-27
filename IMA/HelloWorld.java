538f3bdf7021330a724104a0505054d64c7b53ae
ArfanRahmanYudiantoro <arfanrahman@student.telkomuniversity.ac.id>
2021-09-27 13:28:47 +0700

    HelloWorld.java

---
+++ IMA/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+ public static void main (String[] args) {
+   System.out.println("Hello Arfan Rahman Yudiantoro") ;
+   System.out.println("Nim: 6706213037") ;
+   System.out.println("Kelas: 45-01") ;
+  }
+}
